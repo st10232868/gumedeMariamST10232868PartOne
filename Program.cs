74e79c392c19fab437b4dbf9611ba373ea17f190
Mimi <smimi3347@gmail.comm>
2023-04-22 12:28:03 +0200

    setting and displaying foreground colour of the heading   to Dark Magenta

--- Program.cs
+++ Program.cs
@@ -10,6 +10,15 @@ namespace gumedeMariamST10232868PartOne
     {
         static void Main(string[] args)
         {
+            // Set the Foreground color to DarkMagenta
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            // Display Foreground color DarkMagenta
+            Console.Write("{0}\n  **  WELCOME TO THE RECIPE COMMAND LINE APPLICATION!! ** \n{0} "
+                    , "=======================================================================",Console.ForegroundColor);
+
+
+
+            Console.ReadKey();
         }
     }
 }
