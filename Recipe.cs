58c3f41266cab984abc99b2716c80e80142e3166
Mimi <smimi3347@gmail.comm>
2023-04-24 16:44:14 +0200

    creating a method that adds an ingredient

--- Recipe.cs
+++ Recipe.cs
@@ -10,13 +10,42 @@ namespace gumedeMariamST10232868PartOne
     {   //creating arraylist
         private List<IngredientData> ingredients;
         private List<string> descriptions;
+        //creating an instance of an object (the IngredientData class)
+        IngredientData ingredient = new IngredientData();
 
-        //creating a default constructor
-        public Recipe() 
+
+        //creating a constructor
+        public Recipe()
         {
+            ingredients = new List<IngredientData>();
+            descriptions = new List<string>();
         }
-        //creating a con
+        //creating a method that adds an ingredient
+        public void AddIngredients()
+        {
+            //prompting the user to enter the number of ingredients
+            Console.Write("Please enter the number of ingredients: ");
+            int numOfIngredients = Convert.ToInt32(Console.ReadLine());
+            //declaring and prompting the user to enter the name of the recipe
+            Console.WriteLine("Pleasen enter the name of the recipe");
+            String recipeName = Console.ReadLine();
+            //creating a for loop 
+            for (int i = 0; i < numOfIngredients; i++)
+            {
+                //prompting the user to enter the name of the ingredients
+                Console.Write("Please enter the name of the ingredient: ");
+                //calling the setter and setting the ingredient name to user input
+                ingredient.IngredientName = Console.ReadLine();
+                //prompting the user to enter the quantity of the ingredients    
+                Console.Write("Please enter the quantity of the ingredient: ");
+                ingredient.Quantity = Convert.ToInt32(Console.ReadLine());
+                //prompting the user to enter the unit of measurement
+                Console.Write("Please enter the unit of measurment: ");
+                ingredient.MeasurementUnit = Console.ReadLine();
 
 
+            }
+        }
     }
 }
+
